349bc18412ad6980808c11a73ca1ff2fd90151a3
Frank Stepanski <fsjay@hotmail.com>
2021-06-22 22:02:27 -0400

    setup boilerplate

---
+++ server/index.js
@@ -0,0 +1,14 @@
+// note: server is needed only to statically serve the files created by webpack
+
+const express = require("express");
+const PORT = process.env.PORT || 3000; 
+const path = require('path');
+
+const DIST_PATH = path.join(__dirname, '../dist');
+const app = express();
+
+app.use(express.static(DIST_PATH));
+
+app.listen(PORT, () => {
+    console.log(`server is listening on PORT ${PORT}`);
+})
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const config = {
+    entry: './client/index.js',
+    output: {
+        path: path.resolve(process.cwd(), 'dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            // out of the box, webpack only understands JavaScript files
+            // a loader converts the file to a valid module
+            // loaders enable you to use all kinds of file types
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                      presets: [
+                        ['@babel/preset-env', {
+                          "targets": "defaults" 
+                        }],
+                        '@babel/preset-react'
+                      ]
+                    }
+                  },]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.svg$/,
+                loader: 'url-loader',
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader',
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            title: 'React Webpack Boilerplate',
+            template: path.resolve(__dirname, './template/index.html')
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].bundle.css',
+            chunkFilename: '[id].css'
+          })
+    ]
+}
+
+module.exports = config;
