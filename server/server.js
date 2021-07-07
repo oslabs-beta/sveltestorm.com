1fa7aab4b9959e4cc3ca71955d98a34d0b19d0c2
Sam Filip <samjfilip@gmail.com>
2021-07-06 18:38:45 -0700

    attempted to configure webpack dev server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
-// note: server is needed only to statically serve the files created by webpackconst express = require("express");
 const PORT = process.env.PORT || 3000; 
 const path = require('path');
 const express = require('express');
 
 const DIST_PATH = path.join(__dirname, '../dist');
 const app = express();
-console.log(DIST_PATH);
-app.use(express.static(DIST_PATH));
+app.use(express.urlencoded({ extended: true }));
+
+console.log(DIST_PATH)
+app.use(express.static(__dirname + "./dist/bundle"));
+
+app.use('/', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, '../index.html'))
+  });
 
 app.listen(PORT, () => {
     console.log(`server is listening on PORT ${PORT}`);
